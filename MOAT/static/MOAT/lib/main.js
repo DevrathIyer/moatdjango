8d10a2254af8fd9dd77af4e15239e5326c9b7152
Devrath Iyer <iyer.devrath@gmail.com>
2019-07-03 14:40:14 -0400

    moved assignment ids

--- MOAT/static/MOAT/lib/main.js
+++ MOAT/static/MOAT/lib/main.js
@@ -276,8 +276,8 @@ var test = function()
 
 $(document).ready(function() {
   //get MTurk Form from index.html and populate it with mturk data
-  MTurkForm = $('#TurkForm');
-  MTurkForm.action = URL;
+  //MTurkForm = $('#TurkForm');
+  //MTurkForm.action = URL;
   //MTurkForm['assignmentId'] = assignmentID;
   //get drawing variables
   var canvas = document.getElementById('board');
