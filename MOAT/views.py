feb453ccade5667c11960b3adfde2ab63fcba30b
Devrath Iyer <iyer.devrath@gmail.com>
2019-07-03 11:51:57 -0400

    passed variables to template

--- MOAT/views.py
+++ MOAT/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render('index.html')
+    context = {
+    'exper':request.GET.get('exper', '1'),
+    'assignmentId':request.GET.get('assignmentId', 'NULL'),
+    'prac':request.GET.get('prac', '0'),
+    'agents':request.GET.get('agents', '5')}
+    return render(request,'index.html',context)
