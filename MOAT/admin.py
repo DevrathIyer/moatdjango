7e2063afddd74850f1e3c1348374c932db7c83af
devrathiyer <iyer.devrath@gmail.com>
2020-01-07 13:22:11 -0700

    listview for workers

--- MOAT/admin.py
+++ MOAT/admin.py
@@ -6,3 +6,10 @@ from .models import Experiment, DataPoint, Worker
 admin.site.register(Experiment)
 admin.site.register(DataPoint)
 admin.site.register(Worker)
+
+class ExperimentAdmin(admin.ModelAdmin):
+    fields = ['id', 'workers']
+    list_display = ('get_workers', 'vendor')
+
+    def get_workers(self, obj):
+        return "\n".join([p.workers for p in obj.workers.all()])
