60a3c2df8719e7577f3f047f322ff3b5b35051f3
devrathiyer <iyer.devrath@gmail.com>
2020-01-07 18:56:17 -0700

    added suit

--- MOAT/admin.py
+++ MOAT/admin.py
@@ -1,15 +1,19 @@
 from django.contrib import admin
 from .models import Experiment, DataPoint, Worker
 # Register your models here.
+from django.contrib.auth.models import Group,User
 
 
-admin.site.register(Experiment)
+
 admin.site.register(DataPoint)
 admin.site.register(Worker)
-
+admin.site.unregister(User)
+admin.site.unregister(Group)
 class ExperimentAdmin(admin.ModelAdmin):
     fields = ['id', 'workers']
-    list_display = ('get_workers', 'vendor')
+    #list_display = ('get_workers')
 
     def get_workers(self, obj):
         return "\n".join([p.workers for p in obj.workers.all()])
+
+admin.site.register(Experiment,ExperimentAdmin)
--- moatdjango/settings.py
+++ moatdjango/settings.py
@@ -30,6 +30,8 @@ DEBUG = True
 # Application definition
 
 INSTALLED_APPS = [
+    'suit',
+    #'MOAT.apps.SuitConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
