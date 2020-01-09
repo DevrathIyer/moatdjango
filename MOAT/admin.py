3199b67e8d40ae5a3f82aeba43587f610a0ab111
devrathiyer <iyer.devrath@gmail.com>
2020-01-09 14:37:37 -0700

    OKAY MAYBE THIS TIME

--- MOAT/admin.py
+++ MOAT/admin.py
@@ -23,7 +23,7 @@ class MyAdminSite(admin.AdminSite):
 
 class ExperimentAdmin(admin.ModelAdmin):
     def change_view(self, request, object_id, form_url='', extra_context=None):
-        self.change_view_template = 'Admin/experiment/change_form.html'
+        self.change_view_template = 'Admin/MOAT/experiment/change_form.html'
         logger.info(object_id)
         exp = Experiment.objects.get(pk=object_id)
 
@@ -33,6 +33,7 @@ class ExperimentAdmin(admin.ModelAdmin):
         extra_context['object_id'] = object_id
 
         return super(ExperimentAdmin, self).change_view(request,object_id, form_url='', extra_context=extra_context)
+        #return render(request,'Admin/MOAT/experiment/change_form.html',extra_context)
 mysite = MyAdminSite()
 admin.site = mysite
 sites.site = mysite
