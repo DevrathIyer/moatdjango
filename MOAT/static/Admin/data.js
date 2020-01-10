0ff66d90ecf022da246399345d9e5fd62582fd8e
devrathiyer <iyer.devrath@gmail.com>
2020-01-10 14:56:03 -0700

    okay i actually did it this time

--- MOAT/static/Admin/data.js
+++ MOAT/static/Admin/data.js
@@ -1,7 +1,7 @@
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    console.log(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment/").pop());
+    url = ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment/").pop());
     var datasock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment/").pop());
     datasock.onmessage = function(message) {
         var data = JSON.parse(message.data);
