dd3e9f4375775437de303de0ff2f9cdba06c1f49
devrathiyer <iyer.devrath@gmail.com>
2020-01-10 11:21:47 -0700

    added asgi

--- moatdjango/asgi.py
+++ moatdjango/asgi.py
@@ -2,6 +2,6 @@ import os
 import django
 from channels.routing import get_default_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_channels_heroku.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "moatdjango.settings")
 django.setup()
 application = get_default_application()
