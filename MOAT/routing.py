3287169d622a248260a08791b3b138f3b425cf9b
devrathiyer <iyer.devrath@gmail.com>
2020-01-10 11:09:05 -0700

    updated url

--- MOAT/routing.py
+++ MOAT/routing.py
@@ -1,8 +1,8 @@
 # chat/routing.py
-from django.urls import re_path
+from django.urls import path
 
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'admin/getWorkerData/<slug:experiment_id>/<slug:worker_id>/$', consumers.DataConsumer),
+    path(r'admin/getWorkerData/<slug:experiment_id>/<slug:worker_id>/', consumers.DataConsumer),
 ]
--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 web: gunicorn moatdjango.wsgi
+web: daphne MOAT.asgi:channel_layer --port $PORT --bind 0.0.0.0
