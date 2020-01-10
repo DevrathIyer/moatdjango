e2a78bebf2b11226a199758a7c4da1216f697978
devrathiyer <iyer.devrath@gmail.com>
2020-01-10 14:52:25 -0700

    fixed double slash

--- MOAT/static/Admin/data.js
+++ MOAT/static/Admin/data.js
@@ -1,8 +1,8 @@
 $(function() {
     // When we're using HTTPS, use WSS too.
     var ws_scheme = window.location.protocol == "https:" ? "wss" : "ws";
-    console.log(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment").pop());
-    var datasock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment").pop());
+    console.log(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment/").pop());
+    var datasock = new ReconnectingWebSocket(ws_scheme + '://' + window.location.host + "/admin/getWorkerData" + window.location.pathname.split("/admin/experiment/").pop());
     datasock.onmessage = function(message) {
         var data = JSON.parse(message.data);
         console.log(data)
