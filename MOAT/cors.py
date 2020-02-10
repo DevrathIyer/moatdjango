5b3c8217674a4a45e851962fff150c58b1859428
devrathiyer <iyer.devrath@gmail.com>
2020-02-10 12:56:26 -0700

    added process request

--- MOAT/cors.py
+++ MOAT/cors.py
@@ -1,4 +1,15 @@
+from django.http import HttpResponse
 class CorsMiddleware(object):
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        return self.get_response(request)
+
+    def process_exception(self, request, exception):
+        return HttpResponse("in exception")
+
     def process_response(self, req, resp):
-        response["Access-Control-Allow-Origin"] = "*"
-        return response
+        resp["Access-Control-Allow-Origin"] = "*"
+        return resp
