d2c91a97dce69750257a4a37f3bb1f4ca2658205
devrathiyer <iyer.devrath@gmail.com>
2020-10-19 21:41:16 -0700

    actually committed models.py change

--- MOAT/models.py
+++ MOAT/models.py
@@ -6,9 +6,11 @@ from django.core.exceptions import ValidationError
 from django.dispatch import receiver
 from django.db.models.signals import pre_save, post_save
 
+"""
 @receiver(pre_save)
 def pre_save_handler(sender, instance, *args, **kwargs):
     instance.full_clean()
+"""
 
 class Experiment(models.Model):
     id = models.SlugField(primary_key=True)
